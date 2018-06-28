a249f72b5d91a3a924262d39676b0de266dd058a
ramesh1405 <mr.ramesh1405@gmail.com>
2018-06-28 08:23:51 +0530

    add a class CLineRunner.java

---
+++ SBoot/src/main/java/com/spring/boot/clr/CLineRunner.java
@@ -0,0 +1,14 @@
+package com.spring.boot.clr;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class CLineRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("hello");
+		
+	}
+	
+
+}
